c7e79a39658d7a85aefa07b4c39d26ec112f11ec
Aravin <4869265+Aravin@users.noreply.github.com>
2023-03-24 19:53:43 +0530

    Fixed about page title issue

--- lib/screens/static/about.dart
+++ lib/screens/static/about.dart
@@ -7,7 +7,7 @@ class AboutScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Saved Items')),
+      appBar: AppBar(title: const Text('About Warranty Manager')),
       body: Padding(
         padding: kAppEdgeInsets,
         child: Column(
