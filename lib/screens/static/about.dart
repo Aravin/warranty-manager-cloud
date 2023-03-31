d93cd2a9492493a33bbaee024b16e0ed447810a9
Aravin <4869265+Aravin@users.noreply.github.com>
2023-03-31 23:53:04 +0530

    Updated about us screen

--- lib/screens/static/about.dart
+++ lib/screens/static/about.dart
@@ -1,6 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:warranty_manager_cloud/shared/constants.dart';
+import "package:velocity_x/velocity_x.dart";
 
 class AboutScreen extends StatelessWidget {
   const AboutScreen({super.key});
@@ -9,26 +10,54 @@ class AboutScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('about').tr()),
-      body: Padding(
-        padding: kAppEdgeInsets,
-        child: Column(
-          children: const [
-            Text(
-              'Warranty Manager',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 25.0,
+      body: Center(
+        child: Padding(
+          padding: kAppEdgeInsets,
+          child: Column(
+            children: [
+              const Text(
+                'Warranty Manager',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25.0,
+                ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              'This is Free to use Application. You can store the warranty information of any of your product / service. All your information are stored in your device locally.',
-            ),
-            SizedBox(height: 20),
-            Text('Logo Credit: Viknesh Karthi'),
-          ],
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                'This is Free & open source Application. You can store the warranty information of any of your product / service. All your information are stored in your device locally.',
+              ),
+              const SizedBox(height: 20),
+              const Text('Logo Credit:').text.bold.make(),
+              const SizedBox(height: 5),
+              const Text('Viknesh Karthi'),
+              const Text('https://www.linkedin.com/in/vikneshkarthi/'),
+              const SizedBox(height: 20),
+              const Text('Icons Credit:').text.bold.make(),
+              const SizedBox(height: 5),
+              const Text('https://icons8.com/'),
+              const Text('https://undraw.co/'),
+              const SizedBox(height: 20),
+              const Text('Translations Credit:').text.bold.make(),
+              const SizedBox(height: 5),
+              const Text('https://openai.com/blog/chatgpt'),
+              const SizedBox(height: 20),
+              const Text('Source Code:').text.bold.make(),
+              const SizedBox(height: 5),
+              const Text('https://github.com/Aravin/warranty-manager-cloud'),
+              const SizedBox(height: 20),
+              const Text('Bug Report:').text.bold.make(),
+              const SizedBox(height: 5),
+              const Text(
+                  'https://github.com/Aravin/warranty-manager-cloud/issues'),
+              const SizedBox(height: 20),
+              const Text('New Feature Request:').text.bold.make(),
+              const SizedBox(height: 5),
+              const Text(
+                  'https://github.com/Aravin/warranty-manager-cloud/issues')
+            ],
+          ),
         ),
       ),
     );
