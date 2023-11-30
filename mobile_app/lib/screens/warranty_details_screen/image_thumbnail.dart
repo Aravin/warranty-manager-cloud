4da64ed4ac6f7e08eb742f4d188ebdf9bfacd378
Aravin <4869265+Aravin@users.noreply.github.com>
2023-11-30 14:14:15 +0530

    Added card to thumbnail image

--- mobile_app/lib/screens/warranty_details_screen/image_thumbnail.dart
+++ mobile_app/lib/screens/warranty_details_screen/image_thumbnail.dart
@@ -10,37 +10,39 @@ class ImageThumbnailWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        const SizedBox(height: 25),
-        Center(
-          child: Text(
-            imageName,
-            style: const TextStyle(fontWeight: FontWeight.bold),
+    return Card(
+      child: ListView(
+        children: [
+          const SizedBox(height: 25),
+          Center(
+            child: Text(
+              imageName,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
           ),
-        ),
-        const SizedBox(height: 5),
-        GestureDetector(
-          child: Image.network(
-            image,
-            frameBuilder: (context, child, frame, wasSynchronouslyLoaded) =>
-                child,
-            loadingBuilder: (context, child, loadingProgress) =>
-                loadingProgress == null ? child : appLoader,
-            fit: BoxFit.cover,
-          ),
-          onTap: () => Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (ctx) => DisplayImage(
-                image: image,
-                imageName: imageName,
+          const SizedBox(height: 5),
+          GestureDetector(
+            child: Image.network(
+              image,
+              frameBuilder: (context, child, frame, wasSynchronouslyLoaded) =>
+                  child,
+              loadingBuilder: (context, child, loadingProgress) =>
+                  loadingProgress == null ? child : appLoader,
+              fit: BoxFit.contain,
+            ),
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (ctx) => DisplayImage(
+                  image: image,
+                  imageName: imageName,
+                ),
               ),
             ),
           ),
-        ),
-        const SizedBox(height: 5),
-      ],
+          const SizedBox(height: 5),
+        ],
+      ),
     );
   }
 }
