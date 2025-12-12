6f07c80180049892e948ca1e592617977f859834
Aravind A <4869265+Aravin@users.noreply.github.com>
2025-12-12 19:43:05 +0530

    feat: Update Android compile SDK to 36, Firebase BOM to 33.7.0, switch Firebase dependencies from KTX to non-KTX, and remove `.env` from assets.

--- mobile_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -24,6 +24,7 @@ import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
 import url_launcher_macos
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -45,4 +46,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
